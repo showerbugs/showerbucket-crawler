a914054893ebc8c53c5ab10e066e5a3505305b7d
최민호/협업시스템개발팀/NE <minho-choi@nhnent.com>
2017-05-14 14:10:38 +0900

    first go web preview

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+  "github.com/badoux/goscraper"
+	"net/http"
+	"encoding/json"
+)
+
+type PREVIEW struct {
+	title string
+	description string
+}
+
+func getPreview(w http.ResponseWriter, r *http.Request) {
+		s, err := goscraper.Scrape(r.URL.Query().Get("url"), 5)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		json.NewEncoder(w).Encode(s)
+}
+
+func main() {
+    http.HandleFunc("/", getPreview)
+    http.ListenAndServe(":18088", nil)
+}
