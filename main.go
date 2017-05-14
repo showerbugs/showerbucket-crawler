cdc96d746d5092031956cb31833f450f1c11786a
qodot <waitingforqodot@gmail.com>
2017-05-14 19:45:16 +0900

    Tab -> Space

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"encoding/json"
-	"github.com/badoux/goscraper"
+    "fmt"
+    "net/http"
+    "encoding/json"
+    "github.com/badoux/goscraper"
 )
 
-type PREVIEW struct {
-	title string
-	description string
-}
-
 func getPreview(w http.ResponseWriter, r *http.Request) {
-	s, err := goscraper.Scrape(r.URL.Query().Get("url"), 5)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	json.NewEncoder(w).Encode(s)
+    url := r.URL.Query().Get("url")
+
+    s, err := goscraper.Scrape(url, 5)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    json.NewEncoder(w).Encode(s)
 }
 
 func main() {
-	http.HandleFunc("/", getPreview)
-	http.ListenAndServe(":9090", nil)
+    http.HandleFunc("/", getPreview)
+    http.ListenAndServe(":9090", nil)
 }
